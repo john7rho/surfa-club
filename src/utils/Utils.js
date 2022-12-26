566627ace93f20ccc1f0dd2983e9165c2bda5af8
andrew <asc269@cornell.edu>
2022-12-26 00:33:44 -0600

    Add url retriever util function - returns object url from s3 bucket given name

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -7,6 +7,31 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.REACT_APP_S3_SECRET_ACCESS_KEY,
 });
 
+/**
+ * Gives the url of a object in s3
+ * @param {string} path name of object
+ */
+export const getObjectUrl = async (path) => {
+  s3.getSignedUrl(
+    'getObject',
+    {
+      Bucket: 'surfaclub',
+      Key: path,
+    },
+    (err, url) => {
+      if (err) {
+        throw Error;
+      } else {
+        return url;
+      }
+    },
+  );
+};
+
+/**
+ * uploads file object to s3
+ * @param {File} file
+ */
 export const fileUpload = async (file) => {
   s3.putObject(
     {
@@ -17,14 +42,19 @@ export const fileUpload = async (file) => {
     (err, data) => {
       if (err) {
         console.log(err);
-      } else {
-        console.log(data);
       }
     },
   );
 };
 
-export const register = async ({ username, password, school }) => {
+/**
+ * Registers user and dumps into dynamodb
+ * @param {string} username
+ * @param {string} password
+ * @param {string} school
+ * @param {string} imageUrl
+ */
+export const register = async ({ username, password, school, imageUrl }) => {
   const endpoint =
     'https://70tigy27h2.execute-api.us-east-1.amazonaws.com/prod/register';
 
@@ -32,6 +62,7 @@ export const register = async ({ username, password, school }) => {
   searchParams.set('username', username);
   searchParams.set('password', password);
   searchParams.set('school', school);
+  searchParams.set('image', imageUrl);
 
   fetch(`${endpoint}?${searchParams}`)
     .then(() => console.log('do something when registration succeeds'))
