209c3f37bbac24a79342216838bf22e9324e4cfc
andrew <asc269@cornell.edu>
2022-12-27 16:25:15 -0600

    Add wscat

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,27 @@
 /* eslint-disable no-undef */
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
-    'eslint:recommended',
-  ],
-  'settings': {
-    'react': {
-      'version': 'detect',
+  extends: ['eslint:recommended'],
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'rules': {
-    'indent': [
-      'error',
-      2,
-      { 'SwitchCase': 1 },
-    ],
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    'quotes': [
-      'error',
-      'single',
-    ],
-    'semi': [
-      'error',
-      'always',
-    ],
-    'comma-dangle': [
-      'error',
-      'only-multiline',
-    ],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'only-multiline'],
   },
 };
